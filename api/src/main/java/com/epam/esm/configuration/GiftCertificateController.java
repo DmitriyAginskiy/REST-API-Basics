58fba357efad67a58b3d39a230a0f87b92ad31bb
DimochKaaa <zaaz288@mail.ru>
2021-06-15 18:26:29 +0300

    updated database configuration file

--- api/src/main/java/com/epam/esm/configuration/GiftCertificateController.java
+++ api/src/main/java/com/epam/esm/configuration/GiftCertificateController.java
@@ -1,13 +1,10 @@
-package com.epam.esm.controller;
+package com.epam.esm.configuration;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/certificates")
---
+++ persistence/src/main/java/com/epam/esm/configuration/DatabaseConfiguration.java
@@ -0,0 +1,10 @@
+package com.epam.esm.configuration;
+
+public class DatabaseConfiguration {
+    private static final String PROPERTIES_PATH = "/database.properties";
+    private static final String DRIVER_CLASSNAME = "db.driver";
+    private static final String DATABASE_USER = "db.user";
+    private static final String DATABASE_PASSWORD = "db.password";
+    private static final String DATABASE_POOL_SIZE = "db.pool_size";
+    private static final String DATABASE_URL = "db.url";
+}
